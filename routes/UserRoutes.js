c1893762c9e08b9ae883e772d3d1225d5d43b121
krahul2024 <sandipkumar2024@gmail.com>
2023-06-15 13:06:29 +0530

    first commit

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../models/UserModel.js')
 const crypt = require('crypto')  
 const jwt = require('jsonwebtoken')
+const User = require('../models/UserModel.js')
 const settings = require('../config/settings.js')
 
 // calculating the hash
@@ -11,6 +11,7 @@ const getHash = (text) => {
     hash.update(text) 
     return hash.digest('hex')
 }
+// console.log(getHash('thisisjwtsecret'))
 
 const time = 7 * 24 * 60 * 60 * 1000
 const createToken = async(userId) => {
@@ -27,7 +28,7 @@ router.get('/', async (req, res) => {
 router.post('/signup', async (req, res) => {
 
     const { firstName, lastName, email, password, confirmPassword, department, phoneNumber } = req.body
-    // try {
+    try {
         let user = await User.findOne({ email })
         if (user) return res.status(400).send({
             status: "failure",
@@ -38,7 +39,7 @@ router.post('/signup', async (req, res) => {
             firstName,
             lastName,
             email,
-            password:getHash(password)
+            password:getHash(password),
             confirmPassword:getHash(confirmPassword),
             department,
             phoneNumber
@@ -54,13 +55,13 @@ router.post('/signup', async (req, res) => {
             status: "success",
             message: "User registered successfully"
         })
-    // } catch (error) {
-    //     console.log({ error })
-    //     return res.status(500).send({
-    //         status: "failure",
-    //         message: 'An error occured!'
-    //     })
-    // }
+    } catch (error) {
+        console.log({ error })
+        return res.status(500).send({
+            status: "failure",
+            message: 'An error occured!'
+        })
+    }
 
 })
 
@@ -82,7 +83,7 @@ router.post('/login', async (req, res) => {
         // case of correct password 
         else if (user.password === getHash(password)) {
             // token generation and setting the cookie
-            const token = await createToken(userId)
+            const token = await createToken(user._id)
             res.cookie('accessToken', token, {
                 withCredentials: true,
                 httpOnly: true,
