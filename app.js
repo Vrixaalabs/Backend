cfc4fd9a6f6ff3847b9fbbf79c623bec942fc1d6
ayushman101 <amansinghayush@gmail.com>
2023-07-02 14:57:56 +0530

    environment variables added for security

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser') 
 const cors = require('cors')
+
+require('dotenv').config();
+
 const userRouter = require('./routes/UserRoutes.js')
 const adminRouter = require('./routes/AdminRoutes.js')
 const User = require('./models/UserModel.js')
+
 
 const app = express()
 app.use(cookieParser())
@@ -14,13 +18,25 @@ app.use(express.json()) // for using json format and other stuff related to idk
 
 
 mongoose.set('strictQuery', false)
-mongoose.connect('mongodb://0.0.0.0:27017/internDB', { useNewUrlParser: true })
-    .then(() => console.log(`Connected to the database successfully!`))
-    .catch((error) => {
-        console.log(`There was an error connecting to the database. ${error}`) // printing the error
-    })
+
 app.use('/api', userRouter)
 app.use('/api/admin', adminRouter)
 
 
-const server = app.listen(5000, () => console.log(`Server is running on the PORT: ${5000}`))
+const server = async ()=>{
+
+    try {
+        mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+    .then(() => console.log(`Connected to the database successfully!`))
+    .catch((error) => {
+        console.log(`There was an error connecting to the database. ${error}`) // printing the error
+    })
+
+    app.listen(process.env.PORT, () => console.log(`Server is running on the PORT: ${process.env.PORT}`))
+
+
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
